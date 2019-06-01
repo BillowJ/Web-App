13f81dad54cf725f917ba2cabc5ed5c6a6736287
BillowJ <BillowJinG@163.com>
2019-06-02 00:31:51 +0800

    2019.6.2 0:31

--- www/handlers.py
+++ www/handlers.py
@@ -5,22 +5,45 @@
 import re, time, json, logging, hashlib, base64, asyncio
 
 from coroweb import get, post
-
+from apis import Page, APIValueError, APIResourceNotFoundError
 from models import User, Comment, Blog, next_id
 
+# @get('/')
+# async def index(request):
+#     summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+#     blogs = [
+#         Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+#         Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+#         Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+#     ]
+#     return {
+#         '__template__': 'blogs.html',
+#         'blogs': blogs
+#     }
+def get_page_index(page_str):
+    p = 1
+    try:
+        p = int(page_str)
+    except ValueError as e:
+        pass
+    if p < 1:
+        p = 1
+    return p
+
 @get('/')
-async def index(request):
-    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-    blogs = [
-        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
-        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
-        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
-    ]
+async def index(*, page='1'):
+    page_index = get_page_index(page)
+    num = await Blog.findNumber('count(id)')
+    p = Page(num, page_index)
+    if num == 0:
+        blogs = []
+    else:
+        blogs = await Blog.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
     return {
         '__template__': 'blogs.html',
+        'page': p,
         'blogs': blogs
     }
-
 # @get('/api/users')
 # def api_get_users(*, page='1'):
 #     page_index = get_page_index(page)
